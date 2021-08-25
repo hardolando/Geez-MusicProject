78960bb61a44b8cb9a7835841c3142b2b93f9b34
I'm Papanda <86427308+hardolando@users.noreply.github.com>
2021-08-25 17:08:22 +0700

    Update admins.py

--- GeezProject/modules/admins.py
+++ GeezProject/modules/admins.py
@@ -1,6 +1,6 @@
 # Calls Music 1 - Telegram bot for streaming audio in group calls
-# Copyright (C) 2021  VckyouuBitch
-# Recode By VICKY From Geez-MusicProject
+# Copyright (C) 2021  papanda
+# Recode By papanda From wekawekaland
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -39,7 +39,7 @@ async def update_admin(client, message: Message):
             for member in await message.chat.get_members(filter="administrators")
         ],
     )
-    await message.reply_text("❇️ Admin cache refreshed!")
+    await message.reply_text("List admin udah gue perbarui ye tot!")
 
 
 @Client.on_message(command("pause") & other_filters)
@@ -50,10 +50,10 @@ async def pause(_, message: Message):
     if (chat_id not in callsmusic.pytgcalls.active_calls) or (
         callsmusic.pytgcalls.active_calls[chat_id] == "paused"
     ):
-        await message.reply_text("❗ Nothing is playing!")
+        await message.reply_text("bentar gua cek...")
     else:
         callsmusic.pytgcalls.pause_stream(chat_id)
-        await message.reply_text("▶️ Paused!")
+        await message.reply_text("SIP UDAH GUA PAUSE!")
 
 
 @Client.on_message(command("resume") & other_filters)
@@ -64,10 +64,10 @@ async def resume(_, message: Message):
     if (chat_id not in callsmusic.pytgcalls.active_calls) or (
         callsmusic.pytgcalls.active_calls[chat_id] == "playing"
     ):
-        await message.reply_text("❗ Nothing is paused!")
+        await message.reply_text("Bentar gua cek dulummm")
     else:
         callsmusic.pytgcalls.resume_stream(chat_id)
-        await message.reply_text("⏸ Resumed!")
+        await message.reply_text("SIP UDAH GUA RESUME")
 
 
 @Client.on_message(command("end") & other_filters)
@@ -76,7 +76,7 @@ async def resume(_, message: Message):
 async def stop(_, message: Message):
     chat_id = get_chat_id(message.chat)
     if chat_id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ Nothing is streaming!")
+        await message.reply_text("Bentar gua cek...")
     else:
         try:
             callsmusic.queues.clear(chat_id)
@@ -84,7 +84,7 @@ async def stop(_, message: Message):
             pass
 
         callsmusic.pytgcalls.leave_group_call(chat_id)
-        await message.reply_text("❌ Stopped streaming!")
+        await message.reply_text("Sip, cape gua nyanyi mulu cok cok")
 
 
 @Client.on_message(command("skip") & other_filters)
@@ -94,7 +94,7 @@ async def skip(_, message: Message):
     global que
     chat_id = get_chat_id(message.chat)
     if chat_id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ Nothing is playing to skip!")
+        await message.reply_text("Bentar gua cek...")
     else:
         callsmusic.queues.task_done(chat_id)
 
@@ -110,7 +110,7 @@ async def skip(_, message: Message):
         skip = qeue.pop(0)
     if not qeue:
         return
-    await message.reply_text(f"- Skipped **{skip[0]}**\n- Now Playing **{qeue[0][0]}**")
+    await message.reply_text(f"Oke udah gua skip **{skip[0]}**\nSekarang ganti lagu **{qeue[0][0]}**")
 
 
 @Client.on_message(filters.command("admincache"))
@@ -123,4 +123,4 @@ async def admincache(client, message: Message):
             for member in await message.chat.get_members(filter="administrators")
         ],
     )
-    await message.reply_text("✅️ **Daftar admin** telah **diperbarui**")
+    await message.reply_text("SIP, ADMIN LIST UDAH GUA PERBARUIN")
